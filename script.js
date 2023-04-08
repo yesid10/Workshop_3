13199f1a96506ad03cf4a49c6beaca37ff125598
vickmerys <carolina@gmail.com>
2023-04-08 17:35:13 -0500

    bug fix

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 
 
 let infoPoke = [];
-console.log("ya estoy enlasada con el javascript"); //hace peticion a la url de api pomemon
- const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon/"
+console.log("ya estoy enlasada con el javascript"); //hace peticion a la url de api pokemon.
+const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon/"
 
- //crear funcion para tener informacion del pokemon//
+//crear funcion para tener informacion del pokemon//
 
-const infoApi = async (url)=>{  //
+const infoApi = async (url) => {  //
     const InfoPokemons = [];
 
-try {
-    const {data} = await axios.get(url); //try hace la peticion a la api se ejecuta cuando no hay errores
-    console.log("estoy en la data:", data.results);
-    return data;
+    try {
+        const { data } = await axios.get(url); //try hace la peticion a la api, se ejecuta cuando no hay errores.
+        console.log("estoy en la data:", data.results);
+        return data;
 
-} catch (error) { //catch se utiliza para el manejo de errores
-    console.log("tienes un error");
-    return [];
+    } catch (error) { //catch se utiliza para el manejo de errores.
+        console.log("tienes un error");
+        return [];
+    }
+
+
 }
 
-
-}
-
-document.addEventListener("DOMContentLoaded", async () =>{
+document.addEventListener("DOMContentLoaded", async () => {
     infoPoke = await infoApi(POKEDEX_URL)
-}) //hacemos pericion a la api, //DOMContentLoaded
+}) //hacemos peticion a la api, //DOMContentLoaded 
